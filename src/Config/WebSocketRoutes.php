4ac023cec9a7ba83db203fdcc697141a483ba903
entrepeneur4lyf <shawn.payments@gmail.com>
2024-10-07 19:54:56 -0400

    feat: add reconnect route and more specific WebSocket routes

--- src/Config/WebSocketRoutes.php
+++ src/Config/WebSocketRoutes.php
@@ -10,15 +10,15 @@ return function (WebSocketRouteCollection $routes) {
     $routes->addRoute('message', [WebSocketController::class, 'onMessage']);
     $routes->addRoute('close', [WebSocketController::class, 'onClose']);
     $routes->addRoute('error', [WebSocketController::class, 'onError']);
-    
+
     // Add reconnection route
     $routes->addRoute('reconnect', [WebSocketController::class, 'onReconnect']);
-    
+
     // Add more specific WebSocket routes
     $routes->addRoute('userEvent', [WebSocketController::class, 'onUserEvent']);
     $routes->addRoute('productUpdate', [WebSocketController::class, 'onProductUpdate']);
     $routes->addRoute('notification', [WebSocketController::class, 'onNotification']);
-    
+
     // Add a catch-all route for unhandled events
     $routes->addRoute('default', [WebSocketController::class, 'onUnhandledEvent']);
 };
